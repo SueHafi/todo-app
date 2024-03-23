1811d516822f17cb22c0f52facb8144bdc3063d4
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-03-23 18:40:24 +0000

    did some error handling and added functionality to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   const [userInput, setUserInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
   function handleUserInputChange(event) {
     const newUserInput = event.target.value;
@@ -12,17 +13,40 @@ function App() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    if (userInput === "") {
+      return;
+    }
+    const newTodo = userInput;
+    setTodos((previousTodos) => [...previousTodos, newTodo]);
+    setUserInput("");
+  }
+
+  function handleRemoveButtonClick(event) {
+    const currentListElement = event.target;
+    setTodos(todos.filter());
+    //filter out the current list element and render the rest of the todos
+    //look up how the filter method works
   }
 
   return (
     <>
       <Header />
       <form onSubmit={handleSubmit}>
-        <input placeholder="Type your first to do" value={userInput} onChange={handleUserInputChange}/>
+        <input
+          placeholder="Type your first to do"
+          value={userInput}
+          onChange={handleUserInputChange}
+        />
         <button>Add</button>
-        <button>Remove</button>
-        <li>{userInput}</li>
       </form>
+      {todos.map((userInput, index) => (
+        <div className="container" key={index}>
+          <li>{userInput}</li>
+          <button onClick={(event) => handleRemoveButtonClick(event)}>
+            Remove
+          </button>
+        </div>
+      ))}
     </>
   );
 }
