e66a71152fb2f749900c6199050d78de5b691ecb
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-03-23 13:53:39 +0000

    added functionality to userInput and prevented default on form submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
+
 function App() {
+  const [userInput, setUserInput] = useState("");
+
+  function handleUserInputChange(event) {
+    const newUserInput = event.target.value;
+    setUserInput(newUserInput);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <>
       <Header />
-      <form>
-        <input placeholder="Type your first to do"/>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Type your first to do" value={userInput} onChange={handleUserInputChange}/>
         <button>Add</button>
         <button>Remove</button>
+        <li>{userInput}</li>
       </form>
     </>
   );
