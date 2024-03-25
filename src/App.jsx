178cb05d2831eb6287631517f7e171099d26e51c
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-03-25 00:06:07 +0000

    all functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 function App() {
   const [userInput, setUserInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [todoCheckStatuses, setTodoCheckStatuses] = useState([]);
 
   function handleUserInputChange(event) {
     const newUserInput = event.target.value;
@@ -18,12 +19,29 @@ function App() {
     }
     const newTodo = userInput;
     setTodos((previousTodos) => [...previousTodos, newTodo]);
+    setTodoCheckStatuses((previousChecks) => [...previousChecks, false]);
     setUserInput("");
   }
 
   function handleDeleteButtonClick(indexToDelete) {
     const filteredTodos = todos.filter((_, i) => i !== indexToDelete);
     setTodos(filteredTodos);
+    const filteredCheckboxes = todoCheckStatuses.filter(
+      (_, i) => i !== indexToDelete
+    );
+    setTodoCheckStatuses(filteredCheckboxes);
+  }
+
+  function handleCheckboxClick(event, index) {
+    if (event.target.checked) {
+      const listOfChecked = [...todoCheckStatuses];
+      listOfChecked[index] = true;
+      setTodoCheckStatuses(listOfChecked);
+    } else {
+      const listOfChecked = [...todoCheckStatuses];
+      listOfChecked[index] = false;
+      setTodoCheckStatuses(listOfChecked);
+    }
   }
 
   return (
@@ -38,11 +56,21 @@ function App() {
         />
         <button>Add</button>
       </form>
-      {todos.map((userInput, index) => (
-        <li className="container" key={index}>
-          <input className='checkbox' type='checkbox'/>
-          {userInput}
-          <button className='btn--delete' onClick={() => handleDeleteButtonClick(index)}>Delete</button>
+      {todos.map((todo, index) => (
+        <li className="container" key={todo}>
+          <input
+            checked={todoCheckStatuses[index]}
+            className="checkbox"
+            type="checkbox"
+            onChange={(event) => handleCheckboxClick(event, index)}
+          />
+          {todo}
+          <button
+            className="btn--delete"
+            onClick={() => handleDeleteButtonClick(index)}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </>
