621a6f57862a8d6bd9363ebe72eaf7da5c136e42
Sue Hafizoglu <suehafizoglu@gmail.com>
2024-03-31 03:42:02 +0100

    finished design of application

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import "./App.css";
 
 function App() {
   const [userInput, setUserInput] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { text: "Example todo: Add another todo", isChecked: true, id: uuidv4() },
+    { text: "Example todo: Do homework", isChecked: false, id: uuidv4() },
+  ]);
 
   function handleUserInputChange(event) {
     const newUserInput = event.target.value;
@@ -31,56 +34,61 @@ function App() {
   }
 
   function handleCheckboxClick(event, index) {
-    // const newTodos = todos.map((todo)=> {
-    //   // const copiedTodo = Object.assign({},todo);
-    //   const copiedTodo = {...todo};
-    //   return copiedTodo;
-    // });
-
-    const newTodos = JSON.parse(JSON.stringify(todos));
+    const newTodos = todos.map((todo) => {
+      const copiedTodo = Object.assign({}, todo);
+      return copiedTodo;
+    });
     newTodos[index].isChecked = event.target.checked;
     setTodos(newTodos);
   }
 
-  console.log(todos);
-
   return (
-    <div className="container">
-      <Header />
-      <form className="todo-input-container" onSubmit={handleSubmit}>
-        <input
-          className="user-input-box"
-          type="text"
-          placeholder="Type your first to do"
-          value={userInput}
-          onChange={handleUserInputChange}
-        />
-        <button className="btn--add">Add</button>
-      </form>
-      {todos.map((todo, index) => (
-        <li className="todos-li" key={todo.id}>
-          <div className="todos-container">
-            <div className="todos-checkbox-container">
-              <input
-                checked={todos[index].isChecked}
-                className="checkbox"
-                type="checkbox"
-                onChange={(event) => handleCheckboxClick(event, index)}
-              />
-              <span className={"fuzzy-bubbles-regular" +`${todo.isChecked ? " line-through-text" : ""}`}>
-                {todo.text}
-              </span>
-            </div>
+    <>
+      <div className="container">
+        <Header />
+        <form className="todo-input-container" onSubmit={handleSubmit}>
+          <input
+            className="user-input-box"
+            type="text"
+            placeholder="Type your first to do"
+            value={userInput}
+            onChange={handleUserInputChange}
+          />
+          <button className="btn--add">Add</button>
+        </form>
+        {todos.map((todo, index) => (
+          <li className="todos-li" key={todo.id}>
+            <input
+              checked={todos[index].isChecked}
+              className="checkbox"
+              type="checkbox"
+              onChange={(event) => handleCheckboxClick(event, index)}
+            />
+            <span
+              className={
+                "todo-text" + `${todo.isChecked ? " line-through-text" : ""}`
+              }
+            >
+              {todo.text}
+            </span>
             <button
               className="btn--delete"
               onClick={() => handleDeleteButtonClick(index)}
             >
               Delete
             </button>
-          </div>
-        </li>
-      ))}
-    </div>
+          </li>
+        ))}
+      </div>
+      <footer className="footer">
+        <p>
+          &copy; 2023 Sue Hafizoglu | See code at{" "}
+          <a className="github-link" href="#">
+            GitHub
+          </a>
+        </p>
+      </footer>
+    </>
   );
 }
 
